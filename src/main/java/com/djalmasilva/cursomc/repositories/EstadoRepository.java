3623eb5d954a93384d605a39b0af5172d5d91f02
Djalma Silva <djalmadsilva@gmail.com>
2023-08-15 05:23:34 -0300

    Estado e Cidade

---
+++ src/main/java/com/djalmasilva/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.djalmasilva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.djalmasilva.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
