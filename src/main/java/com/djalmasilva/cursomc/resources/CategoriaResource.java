7a61c99e4ceef0725f2272b57cb6787d0cad7779
Djalma Silva <djalma@unirp.edu.br>
2023-08-30 06:26:08 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/djalmasilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/djalmasilva/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.djalmasilva.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.djalmasilva.cursomc.domain.Categoria;
 import com.djalmasilva.cursomc.services.CategoriaService;
@@ -17,38 +20,21 @@ import com.djalmasilva.cursomc.services.CategoriaService;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	/*
-	 * @RequestMapping(method = RequestMethod.GET) public String testar() {
-	 * 
-	 * return "REST está funcionando!";
-	 * 
-	 * }
-	 */
-
 	@Autowired
 	private CategoriaService service;
 
-//	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-
-		return lista;
-
-	}
-
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-
-		
+	public ResponseEntity<?> find(@PathVariable Integer id) {	
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
