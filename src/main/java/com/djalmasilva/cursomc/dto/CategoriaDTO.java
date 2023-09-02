afd95961e2f1dc485f4eb39d327fe6390b074bcf
Djalma Silva <djalma@unirp.edu.br>
2023-09-02 11:51:20 -0300

    Validação sintática com Bean Validation, mas que não funcionou

--- src/main/java/com/djalmasilva/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/djalmasilva/cursomc/dto/CategoriaDTO.java
@@ -4,11 +4,17 @@ import java.io.Serializable;
 
 import com.djalmasilva.cursomc.domain.Categoria;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Size(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
