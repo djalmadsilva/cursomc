9c8c6ed12603ee903f33bdc716ab80cbf7116109
Djalma Silva <djalma@unirp.edu.br>
2023-09-11 07:19:15 -0300

    Validação customizada de CPF ou CNP na inserção de Cliente, mas que não funcionaou

--- src/main/java/com/djalmasilva/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/djalmasilva/cursomc/dto/ClienteNewDTO.java
@@ -2,29 +2,50 @@ package com.djalmasilva.cursomc.dto;
 
 import java.io.Serializable;
 
+import com.djalmasilva.cursomc.services.validation.ClienteInsert;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Size(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
-	private String cpfOuCnpj;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	String cpfOuCnpj;
 	private Integer tipo;
-	
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
 	private String bairro;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
-	
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeId;
-	
+
 	public ClienteNewDTO() {
-		
+
 	}
 
 	public String getNome() {
@@ -130,5 +151,5 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
+
 }
---
+++ src/main/java/com/djalmasilva/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.djalmasilva.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.djalmasilva.cursomc.domain.enums.TipoCliente;
+import com.djalmasilva.cursomc.dto.ClienteNewDTO;
+import com.djalmasilva.cursomc.resources.exception.FieldMessage;
+import com.djalmasilva.cursomc.services.validation.utils.BR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
